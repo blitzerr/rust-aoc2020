d32fb35a974569c7b2d9d02d97ce4989f04efedd
blitzerr <rusty.blitzerr@gmail.com>
2020-12-06 15:54:05 -0800

    Day 1 and day 2

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/// https://adventofcode.com/2020/
+/// We are putting together the solutions for advent of code 2020.
+
+mod day1;
+mod day2;
+mod day3;
+
+fn main() {
+    println!("Hello, world!");
+}
